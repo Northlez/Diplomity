dde25443a4e1b08d3d3ef965009cc2b904f49fa7
Northlez <kkonovalenk010@gmail.com>
2020-04-25 22:31:50 +0900

    Some animation

---
+++ Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = 0.1f;
+
+    Animator characterAnimator;
+    NavMeshAgent navAgent;
+    
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        characterAnimator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        float speedPercent = navAgent.velocity.magnitude / navAgent.speed;
+        characterAnimator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
