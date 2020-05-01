bee9f91935c40d3577a0cedefe856d43b74ba52b
Northlez <kkonovalenk010@gmail.com>
2020-05-01 16:14:06 +0900

    Unity Update

---
+++ Assets/Scripts/Items and Inventory/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EquipmentSlot { Head, Chest, Leags, Weapon, Shield, Feet }
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+    public EquipmentSlot equipSlot;
+
+    public int armor;
+    public int damage;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+
+        RemoveFromInventory();
+    }
+
+}
---
+++ Assets/Scripts/Items and Inventory/EquipmentManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static EquipmentManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    Equipment[] currentEquipment;
+
+    public delegate void OnEquipmentChanged(Equipment newEquip, Equipment oldEquip);
+    public OnEquipmentChanged onEquipmentChanged;
+
+    Inventory inventory;
+
+    private void Start()
+    {
+        int numOfSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[numOfSlots];
+        inventory = Inventory.instance;
+    }
+
+    public void Equip(Equipment newEquip)
+    {
+        int slotIndex = (int)newEquip.equipSlot;
+
+        Equipment oldEquip = null;
+
+        if (currentEquipment[slotIndex] != null)
+        {
+            oldEquip = currentEquipment[slotIndex];
+            inventory.Add(oldEquip);
+        }
+
+        if(onEquipmentChanged != null)
+        {
+            onEquipmentChanged.Invoke(newEquip, oldEquip);
+        }
+
+        currentEquipment[slotIndex] = newEquip;
+    }
+
+    public void Unequip(int slotIndex)
+    {
+        if (currentEquipment[slotIndex] != null)
+        {
+            Equipment oldEquip = currentEquipment[slotIndex];
+            inventory.Add(oldEquip);
+
+            currentEquipment[slotIndex] = null;
+
+            if (onEquipmentChanged != null)
+            {
+                onEquipmentChanged.Invoke(null, oldEquip);
+            }
+        }
+    }
+
+}
