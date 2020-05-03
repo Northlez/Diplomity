b62585b3e406d4bdd56d1891ab1394162a9e93c9
Northlez <kkonovalenk010@gmail.com>
2020-05-03 20:21:54 +0900

    Assets and Stats

---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage (int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+        Debug.Log(transform.name + " took " + damage + " damage.");
+    }
+
+    public virtual void Die()
+    {
+        //Персонаж умирает
+        //Метод будет переопределён
+    }
+}
---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Equipment newEquip, Equipment oldEquip)
+    {
+        if (newEquip != null)
+        {
+            armor.AddModifier(newEquip.armor);
+            damage.AddModifier(newEquip.damage);
+        }
+
+        if (oldEquip != null)
+        {
+            armor.RemoveModifier(newEquip.armor);
+            damage.RemoveModifier(newEquip.damage);
+        }
+    }
+}
